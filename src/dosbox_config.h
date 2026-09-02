10808a2aa6a3d070f1948ba70bf602987ea616ba
SteveShi <xuanlian@mac.com>
2026-09-03 04:36:06 +0800

    fix(build): prioritize binary include directory in CMake and guard platform headers in dosbox_config.h

--- src/dosbox_config.h
+++ src/dosbox_config.h
@@ -13,7 +13,9 @@
 #define DOSBOX_VERSION_SHORT "0.83"
 #define BUILD_GIT_HASH "boxer-v0.83.0"
 
+#if defined(__APPLE__)
 #define MACOSX 1
+#endif
 
 #if defined(__aarch64__) || defined(__arm64__)
 #define C_TARGETCPU ARMV8LE
@@ -43,6 +45,7 @@
 #define C_FLUIDSYNTH 0
 #define C_SHOWPIC 0
 
+#if defined(__APPLE__)
 #define C_COREAUDIO 1
 #define C_COREMIDI 1
 #define C_COREFOUNDATION 1
@@ -63,11 +66,13 @@
 
 #define HAVE_LIBGEN_H 1
 #define HAVE_NETINET_IN_H 1
-#define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_SOCKET_H 1
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_XATTR_H 1
+#endif
+
+#define HAVE_STDLIB_H 1
 
 #define CUSTOM_DATADIR ""
 
